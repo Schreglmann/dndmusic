1089010597a71dabf2d236652b6728660361f4da
Georg Schreglmann <georgschreglmann@gmx.at>
2021-06-05 18:48:16 +0200

    Add persistant song storage

--- public/audioPlayerAdmin.js
+++ public/audioPlayerAdmin.js
@@ -1,52 +1,59 @@
 const audioSources = {};
+const currentCategory = "";
 
 fetch("https://dndmusic.schreglmann.at/getFiles?path=music", {
-	credentials: "same-origin",
+  credentials: "same-origin",
 })
-	.then((response) => response.json())
-	.then((data) => {
-		data.forEach((category) => {
-			if (category[0] != ".") {
-				fetch("https://dndmusic.schreglmann.at/getFiles?path=music/" + category, {
-					credentials: "same-origin",
-				})
-					.then((response) => response.json())
-					.then((data) => {
-						audioSources[category] = data;
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((category) => {
+      if (category[0] != ".") {
+        fetch(
+          "https://dndmusic.schreglmann.at/getFiles?path=music/" + category,
+          {
+            credentials: "same-origin",
+          }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            audioSources[category] = data;
 
-						var button = document.createElement("div");
-						button.innerHTML = category;
-						button.className = "musicButtons";
-						button.id = category;
-						button.onclick = () => playAudio(category);
-						var body = document.getElementById("musicButtons");
-						body.appendChild(button);
-					});
-			}
-		});
-	});
+            var button = document.createElement("div");
+            button.innerHTML = category;
+            button.className = "musicButtons";
+            button.id = category;
+            button.onclick = () => playAudio(category);
+            var body = document.getElementById("musicButtons");
+            body.appendChild(button);
+          });
+      }
+    });
+  });
 
 const player = document.getElementById("player");
 function playAudio(category) {
-	let audioSource =
-		category +
-		"/" +
-		audioSources[category][
-			Math.floor(Math.random() * audioSources[category].length)
-		];
-	player.src = audioSource;
+  if (category != currentCategory) {
+    currentCategory = currentCategory;
+    let audioSource =
+      currentCategory +
+      "/" +
+      audioSources[currentCategory][
+        Math.floor(Math.random() * audioSources[currentCategory].length)
+      ];
+    player.src = audioSource;
     writeCurrentSong(audioSource);
-	player.play();
+    player.play();
+  }
 }
 player.addEventListener("ended", playAudio);
 
 function writeCurrentSong(song) {
-	fetch("https://dndmusic.schreglmann.at/writeCurrentSong", {
-		method: "POST",
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ currentSong: song }),
-	});
+  fetch("https://dndmusic.schreglmann.at/writeCurrentSong", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ currentSong: song }),
+  });
 }
