e3979a14e3b1c40d766db911f8e5125d3d440ed4
Georg Schreglmann <georgschreglmann@gmx.at>
2021-06-05 18:53:01 +0200

    Add default value

--- public/audioPlayerAdmin.js
+++ public/audioPlayerAdmin.js
@@ -31,7 +31,7 @@ fetch("https://dndmusic.schreglmann.at/getFiles?path=music", {
   });
 
 const player = document.getElementById("player");
-function playAudio(category) {
+function playAudio(category = '') {
   if (category != currentCategory) {
     currentCategory = category;
     let audioSource =
