773e1a6e621a91861aee3b00442ce886c22d8623
Georg Schreglmann <georgschreglmann@gmx.at>
2021-11-07 16:39:38 +0100

    Change Playlist to be rendered on server, not on client

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const path = require("path");
 const fs = require("fs");
 const basicAuth = require("express-basic-auth");
+const { getAudioDurationInSeconds } = require('get-audio-duration');
 
 app.use(
 	express.urlencoded({
@@ -22,6 +23,9 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "/client.html"));
 });
 app.get("/admin", (req, res) => {
+	res.sendFile(path.join(__dirname, "/adminNew.html"));
+});
+app.get("/admin_old", (req, res) => {
 	res.sendFile(path.join(__dirname, "/admin.html"));
 });
 app.get("/getFiles", (req, res) => {
@@ -49,7 +53,61 @@ app.post("/writeCurrentSong", (req, res) => {
 	});
 });
 
+app.post("/newCategory", (req, res) => {
+    playNewCategory(req.body.category);
+    res.end("success");
+});
+
+app.post("/stop", (req, res) => {
+    stop();
+    res.end("success");
+});
+
 app.use(express.static("music"));
 app.listen(port, () => {
 	console.log(`DnDMusic listening at http://localhost:${port}`);
 });
+
+
+let currentCategory = new Array();
+let currentCategorySongs = new Array();
+let currentSong = '';
+let timeout;
+
+let playNewCategory = category => {
+    if (timeout) clearTimeout(timeout);
+    currentCategory = category;
+    currentCategorySongs = [];
+    fs.readdir('music/' + category, (err, files) => {
+		if (files) {
+			files.forEach((file) => {
+				if (file[0] != ".") currentCategorySongs.push(file);
+			});
+            currentCategorySongs = currentCategorySongs.sort((a, b) => 0.5 - Math.random());
+            playNewSong();
+		}
+    });
+}
+let playNewSong = () => {
+    currentSong = currentCategorySongs[0];
+    currentCategorySongs.shift();
+
+    fs.writeFile("currentSong.txt", currentCategory + '/' + currentSong, () => {});
+
+    getAudioDurationInSeconds('music/' + currentCategory + '/' + currentSong).then(duration => {
+        console.log(currentSong);
+        if (currentCategorySongs.length > 0) timeout = setTimeout(playNewSong, duration*1000);
+        else {
+            timeout = setTimeout(restartPlaylist, duration*1000)
+        }
+    });
+}
+let restartPlaylist = () => {
+    stop();
+    setTimeout(playNewCategory, 2000, currentCategory);
+}
+
+let stop = () => {
+    if (timeout) clearTimeout(timeout);
+	fs.writeFile("currentSong.txt", 'stop', () => {});
+}
---
+++ public/audioPlayerAdminNew.js
@@ -0,0 +1,69 @@
+const audioSources = {};
+let currentAudioSources = {};
+let currentCategory = "";
+let currentSong = "";
+
+fetch("https://dndmusic.schreglmann.at/getFiles?path=music", {
+  credentials: "same-origin",
+})
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((category) => {
+      if (category[0] != ".") {
+        fetch(
+          "https://dndmusic.schreglmann.at/getFiles?path=music/" + category,
+          {
+            credentials: "same-origin",
+          }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            audioSources[category] = data;
+
+            var button = document.createElement("div");
+            button.innerHTML = category;
+            button.className = "musicButtons";
+            button.id = category;
+
+            if (category.includes("Atmosphere"))
+              button.className += " atmosphere";
+            else if (category.includes("Ambience"))
+              button.className += " ambience";
+            else if (category.includes("Combat")) button.className += " combat";
+            else if (category.includes("Feeling"))
+              button.className += " feeling";
+            else if (category.includes("Location"))
+              button.className += " location";
+            else if (category.includes("Stealth"))
+              button.className += " stealth";
+            else if (category.includes("Temple")) button.className += " temple";
+
+            button.onclick = () => playAudio(category);
+            var body = document.getElementById("musicButtons");
+            body.appendChild(button);
+          });
+      }
+    });
+  });
+
+
+function playAudio(category = "") {
+    fetch("https://dndmusic.schreglmann.at/newCategory", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ category: category }),
+    }); 
+}
+
+function stopMusic() {
+    fetch("https://dndmusic.schreglmann.at/stop", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }); 
+}
