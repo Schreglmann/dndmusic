5664c12e94f6f638bbaa2a9b996617b6fb7d248f
Georg Schreglmann <georgschreglmann@gmx.at>
2021-11-13 14:07:58 +0100

    Change Volume Steps

--- public/audioPlayerAdminNew.js
+++ public/audioPlayerAdminNew.js
@@ -15,38 +15,29 @@ fetch(remoteUrl + "/getFiles?path=music", {
   .then((data) => {
     data.forEach((category) => {
       if (category[0] != ".") {
-        fetch(
-          remoteUrl + "/getFiles?path=music/" + category,
-          {
-            credentials: "same-origin",
-          }
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            audioSources[category] = data;
+        var button = document.createElement("div");
+        button.innerHTML = category;
+        button.className = "musicButtons";
+        button.id = category;
 
-            var button = document.createElement("div");
-            button.innerHTML = category;
-            button.className = "musicButtons";
-            button.id = category;
+        if (category.includes("Atmosphere"))
+            button.className += " atmosphere";
+        else if (category.includes("Ambience"))
+            button.className += " ambience";
+        else if (category.includes("Combat"))
+            button.className += " combat";
+        else if (category.includes("Feeling"))
+            button.className += " feeling";
+        else if (category.includes("Location"))
+            button.className += " location";
+        else if (category.includes("Stealth"))
+            button.className += " stealth";
+        else if (category.includes("Temple"))
+            button.className += " temple";
 
-            if (category.includes("Atmosphere"))
-              button.className += " atmosphere";
-            else if (category.includes("Ambience"))
-              button.className += " ambience";
-            else if (category.includes("Combat")) button.className += " combat";
-            else if (category.includes("Feeling"))
-              button.className += " feeling";
-            else if (category.includes("Location"))
-              button.className += " location";
-            else if (category.includes("Stealth"))
-              button.className += " stealth";
-            else if (category.includes("Temple")) button.className += " temple";
-
-            button.onclick = () => playAudio(category);
-            var body = document.getElementById("musicButtons");
-            body.appendChild(button);
-          });
+        button.onclick = () => playAudio(category);
+        var body = document.getElementById("musicButtons");
+        body.appendChild(button);
       }
     });
   });
--- public/audioPlayerCommon.js
+++ public/audioPlayerCommon.js
@@ -1,24 +1,25 @@
-const volumeSteps = 0.05;
+let currentVolumeStep = 0;
+const volumeSteps = new Array (1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.18, 0.16, 0.14, 0.12, 0.1, 0.09, 0.08, 0.07, 0.06, 0.05, 0.04, 0.03, 0.02, 0.01);
 let muted = false;
 
 function changeVolume(higherLower) {
-  if (higherLower == "mute") {
-    if (muted == false) {
-      muted = player.volume;
-      player.volume = 0;
-    } else {
-      player.volume = muted;
-      muted = false;
+    if (higherLower == "mute") {
+        if (muted == false) {
+        muted = player.volume;
+        player.volume = 0;
+        } else {
+        player.volume = muted;
+        muted = false;
+        }
+    } else if (higherLower == "+" && currentVolumeStep != 0) {
+        player.volume = volumeSteps[currentVolumeStep-1];
+        currentVolumeStep --;
+    } else if (higherLower == "-" && currentVolumeStep != volumeSteps.length-1) {
+        player.volume = volumeSteps[currentVolumeStep+1];
+        currentVolumeStep ++;
     }
-  } else if (higherLower == "+" && player.volume + volumeSteps <= 1) {
-    player.volume += volumeSteps;
-  } else if (higherLower == "-" && player.volume - volumeSteps >= 0) {
-    player.volume -= volumeSteps;
-  }
-  document.getElementById("currentVolume").innerHTML =
-    Math.round(player.volume * 100) / 100;
+    document.getElementById("currentVolume").innerHTML = Math.round(player.volume * 100) / 100;
 }
-
 
 let deferredPrompt;
 
@@ -32,9 +33,7 @@ document.getElementById("btnSave").addEventListener("click", function() {
       deferredPrompt.prompt();
   
       deferredPrompt.userChoice.then(function(choiceResult) {
-  
         console.log(choiceResult.outcome);
-  
         if(choiceResult.outcome == 'dismissed') {
           console.log('User cancelled home screen install');
         }
